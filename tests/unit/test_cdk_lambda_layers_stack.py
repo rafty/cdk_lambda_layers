af8b83fa2652529a7466cd37f0add71244942577
yagita <yagita.takashi@gmail.com>
2021-11-29 15:40:01 +0900

    Initial commit

---
+++ tests/unit/test_cdk_lambda_layers_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from cdk_lambda_layers.cdk_lambda_layers_stack import CdkLambdaLayersStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_lambda_layers/cdk_lambda_layers_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = CdkLambdaLayersStack(app, "cdk-lambda-layers")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
